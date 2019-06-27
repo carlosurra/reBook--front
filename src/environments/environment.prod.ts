717ef76256169486c8ffe244c4edc88d1d8d9ab9
carlos <jcurra82@gmail.com>
2019-06-27 19:40:50 +0200

    enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:3000'
+  apiBaseUrl: 'https://git.heroku.com/rebookback.git'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000'
+  apiBaseUrl: 'https://git.heroku.com/rebookback.git'
 };
 
 /*
